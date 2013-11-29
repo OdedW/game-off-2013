f8768ac36643c4ce8fa92ae5f94221da5edd0572
Oded Welgreen <oded_wel@hotmail.com>
2013-11-29 15:04:10 +0200

    fix credits screen

--- SFQ/Scripts/app/CreditsScreen.js
+++ SFQ/Scripts/app/CreditsScreen.js
@@ -11,14 +11,13 @@
                 this.logo.x = 230;
                 this.subItem1 = this.createOutlinedText("• Using the 16-bit set from Oryx Design Labs", 18, 310, 2);
                 this.subItem2 = this.createOutlinedText("www.oryxdesignlab.com", 14, 332, 2);
-                this.subItem3 = this.createOutlinedText("• Created with CreateJS", 18, 360, 2);
-                this.subItem4 = this.createOutlinedText("• Cash register icon by www.fatcow.com", 18, 390, 2);
-                this.subItem5 = this.createOutlinedText("• Thud sound by Speedenza from freesound", 18, 420, 2);
-                this.subItem6 = this.createOutlinedText("Subscribe to our twitter feed for more news!", 18, 450, 2);
-                this.subItem7 = this.createOutlinedText("twitter.com/InjaGames", 18, 480, 2);
-                this.mainView.addChild(this.title, this.logo, this.subItem1, this.subItem2, this.subItem3, this.subItem4, this.subItem5, this.subItem6, this.subItem7);
+                this.subItem3 = this.createOutlinedText("• Created with CreateJS", 18, 370, 2);
+
+                this.subItem4 = this.createOutlinedText("Subscribe to our twitter feed for more news!", 18, 410, 2);
+                this.subItem5 = this.createOutlinedText("twitter.com/InjaGames", 18, 440, 2);
+                this.mainView.addChild(this.title, this.logo, this.subItem1, this.subItem2, this.subItem3, this.subItem4, this.subItem5);
                 this.setupLink(this.subItem2, 'http://www.oryxdesignlab.com');
-                this.setupLink(this.subItem7, 'http://twitter.com/InjaGames');
+                this.setupLink(this.subItem5, 'http://twitter.com/InjaGames');
                 this.mainView.addEventListener("click", function (evt) {
                     
                     if (!that.subItem2.hitTest(that.subItem2.globalToLocal(evt.stageX, evt.stageY).x,that.subItem2.globalToLocal(evt.stageX, evt.stageY).y ) && 
